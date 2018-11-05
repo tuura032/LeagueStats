daf06876e396be41ae35427400f80691e19faa77
Paul Tuura <tuura032@gmail.com>
2018-11-05 15:06:57 -0700

    removed espnff

--- application.py
+++ application.py
@@ -4,7 +4,6 @@ from flask import Flask, session, render_template, request, url_for, flash, redi
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from getScoreboardData import getscorestobeat
 import requests
 
 app = Flask(__name__)
