0f0f82ee39e1aa795a3197d00aa61cde0cd87574
Paul Tuura <tuura032@gmail.com>
2018-11-06 19:08:00 -0700

    major update, added pages, 2nd db, fixed sorting

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
+# TODO I'm struggling with the varchar values of scores_wk_. I might need to just create a new data table (which is fine), or not deal with sorting scores. 
+# also STB udpates only through week 9, I should have it go until it reaches a null value. maybe a while loop instead? Or just add an if statement and break.
+
 import os
 
-from flask import Flask, session, render_template, request, url_for, flash, redirect, jsonify
+from flask import Flask, session, render_template, request, url_for, flash, redirect, jsonify, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from getScoreboardData import getscorestobeat, getweekscores, getpointsfor, getallteamwins
 import requests
+from espnff import League
 
 app = Flask(__name__)
 
@@ -21,70 +26,151 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def home():
-    
     # Query the database for data
-    everything = db.execute("SELECT * FROM ffftable").fetchall()
-    sortedbywin = db.execute("SELECT * FROM ffftable ORDER BY total_wins DESC").fetchall()
+    sortedbywin = db.execute("SELECT * FROM ffftable ORDER BY total_wins DESC, total_points DESC").fetchall()
     
-    # Figure out who has the most points
+    # Get Player with most points
+    mostpf = db.execute("SELECT owner, total_points FROM ffftable ORDER BY total_points DESC").fetchone()
     
-    sortpf = db.execute("SELECT owner, points_for FROM ffftable WHERE LENGTH(points_for) > 5 ORDER BY points_for DESC").fetchall()
-    
-    
-
+    # Get current places
     first = sortedbywin[0]
     second = sortedbywin[1]
     third = sortedbywin[2]
-    mostpf = sortpf[0]
-
     
-    return render_template("home.html", everything=everything, sortedbywin=sortedbywin, first=first, second=second, third=third, mostpf = mostpf)
+    
+    return render_template("home.html", sortedbywin=sortedbywin, first=first, second=second, third=third, mostpf = mostpf)
 
-
-    #orm examples
-    #ffftable.query.all()
-    #ffftable.query.filter_by(colulmn_name = 'something').all() // or .first()
-
-@app.route("/update", methods=["GET", "POST"])
-def update():
-
-    #if request.method == "POST":
-        #update number of top6 wins
-        #scoretobeat = getscorestobeat()
-        scoretobeat = [105.1, 112.4, 107.2, 108.5, 107.5, 103.8, 109.3, 112.3, 0, 0, 0, 0]
-        #scores = 'scores_wk'+str(n)
-        data = db.execute("SELECT * FROM ffftable").fetchall()
+@app.route("/<int:view_week>")
+def homeextra(view_week):
+    # Query the database for data
+    sortedbywin = db.execute("SELECT * FROM ffftable ORDER BY total_wins DESC, total_points DESC").fetchall()
+    
+    # Get Player with most points
+    mostpf = db.execute("SELECT owner, total_points FROM ffftable ORDER BY total_points DESC").fetchone()
+    
+    if view_week != 0 and view_week < 17 and view_week > 0:
+        some_week = db.execute("SELECT * FROM fffscores").fetchall()
         
-        # Loop through each player, and reset variables
-        for player in data:
-            counter=0
-            n=6 # index of db for weekly scoring
-            
-            # Increase counter each week a player surpasses STB
-            for week in range(1,10):
-                
-                # If a player doesn't surpass STB, break
-                if float(player[n]) > scoretobeat[week-1]:
-                    if scoretobeat[week-1] == 0:
-                        break
-                    #db.execute("UPDATE top6 SET top6 = :top6", {"top6": +=1})
-                    counter += 1
-                    #print(player.owner + ": ", end='')
-                    #print(player[n]) #print score from each week
-                    #print("Beat score of: ", end='')
-                    #print(scoretobeat[week-1])
-                    #print("Top 6 wins: ", end='')
-                    #print(counter)
-                n += 1
-            
-            wintotal = player.wins + counter
-            db.execute("UPDATE ffftable SET top6 = :top6, total_wins = :total_wins WHERE id=:id", {"top6": counter, "total_wins": wintotal, "id": player.id })
-            db.commit()
-            print(player.owner + ": ", end='')
-            print(counter, end='') 
-            print(" top6 apperances and ", end='')
-            print(wintotal, end='')
-            print(" total wins!")
+        # make a sorted list of tuples (owner, score)
+        week_scores = []
+        index = view_week + 1
+        
+        # get owner, score for the given week
+        for owner in some_week:
+            score = owner[index]
+            owner = owner.team_name
+            week_scores.append((score, owner))
 
-        return render_template("update.html")
+        # send week number and sorted (score : owner) to data table
+        week_scores.sort(key=lambda tup: tup[0], reverse=True) 
+        week = view_week
+    
+    else:
+        some_week is None
+    
+    
+    return render_template("home.html", sortedbywin=sortedbywin, mostpf = mostpf, some_week=some_week, week_scores=week_scores, week=week)
+
+@app.route("/update", methods=["GET"])
+def update():
+    '''update number of h2h wins, top6 wins and total wins'''
+    '''Don't refresh after games have started'''
+        
+    # Get List of all pf, already ordered by owner id
+    pf_list = getpointsfor()
+    
+    # get scoretobeat (STB) from espnff
+    scoretobeat = getscorestobeat() # else use this: scoretobeat = [105.1, 112.4, 107.2, 108.5, 107.5, 103.8, 109.3, 112.3, 103.5, 0, 0, 0, 0]
+
+    # get weekly scoring data from table
+    data = db.execute("SELECT * FROM ffftable ORDER BY id").fetchall()
+
+    # Get list of all team wins
+    team = getallteamwins()
+    m=0 # index for list of players
+    
+    # Loop through each player in data table, and reset index/counter
+    for player in data:
+        counter=0 # times each player surpasses STB
+        n=6 # index of db for weekly scoring
+        
+        
+        # Calculate number of weeks in top6 scoring
+        for week in range(1,14):
+
+            # Stop counting if player score is 0 or STB is 0
+            if player[n] == "0" or scoretobeat[week-1] == 0:
+                break
+            
+            # If a player doesn't surpass STB, break
+            if float(player[n]) > scoretobeat[week-1]:
+                counter += 1
+
+            #move to next week in index
+            n += 1
+        
+        wintotal = team[m].wins + counter
+        db.execute("UPDATE ffftable SET wins = :wins, top6 = :top6, total_wins = :total_wins, total_points = :pf WHERE id=:id", 
+                    {"wins": team[m].wins, "top6": counter, "total_wins": wintotal, "pf": pf_list[m], "id": player.id })
+        db.commit()
+        print(player.owner + ": ", end='')
+        print(team[m].wins, end='')
+        print(" H2H wins, ", end='')
+        print(counter, end='') 
+        print(" top6 apperances and ", end='')
+        print(wintotal, end='')
+        print(" total wins!")
+        
+        m+=1 # advance index to next list item
+
+    return render_template("update.html")
                     
+@app.route("/weeklyupdate", methods=["GET", "POST"])
+def weeklyupdate():
+    '''udpate db with scores from the week'''
+    '''in week 10, be sure the UPDATE to the scores table is the correct data type'''
+
+    if request.method == "POST":
+        if not request.form.get("week"):
+            return render_template("home.html")
+        else:
+        
+            week = int(request.form.get("week"))
+            listofscores = getweekscores(week)
+            for n in range(0, 12):
+                id = n+1
+                singlescore = str(listofscores[n])
+                floatscore = float(listofscores[n])
+                
+                # Update scores in both tables, given the week
+                if week == 9:
+                    db.execute("UPDATE ffftable SET scores_wk9 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                elif week == 10:
+                    db.execute("UPDATE ffftable SET scores_wk10 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk10_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                elif week == 11:
+                    db.execute("UPDATE ffftable SET scores_wk11 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk11_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                elif week == 12:
+                    db.execute("UPDATE ffftable SET scores_wk12 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk12_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                elif week == 13:
+                    db.execute("UPDATE ffftable SET scores_wk13 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk13_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                elif week == 14:
+                    db.execute("UPDATE ffftable SET scores_wk14 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk14_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                elif week == 15:
+                    db.execute("UPDATE ffftable SET scores_wk15 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk15_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                elif week == 16:
+                    db.execute("UPDATE ffftable SET scores_wk16 = :score WHERE id = :id", {"score": singlescore, "id": id})
+                    db.execute("UPDATE fffscores SET wk16_scores = :score WHERE id = :id", {"score": floatscore, "id": id})
+                else:
+                    return render_template("home.html")
+            db.commit()
+
+            return render_template("weeklyupdate.html")
+    
+    else:
+        return render_template("weeklyupdate.html")
--- getScoreboardData.py
+++ getScoreboardData.py
@@ -8,9 +8,10 @@ league.scoreboard() # grab current week
 scoreboard_main = league.scoreboard(week=1) # define week
 
 def getscorestobeat():
+    '''Get Score to Beat for weeks 1-13'''
     allscorestobeat = []
 
-    for week in range(1, 13):
+    for week in range(1, 14):
         scoreboard = league.scoreboard(week=week)
         tempstorage = []
         for matchups in scoreboard:
@@ -21,5 +22,44 @@ def getscorestobeat():
 
     return allscorestobeat
 
-#x = getscorestobeat()
-#print(x)
+def getweekscores(week_num):
+    '''Get All Scores from a given week in a list'''
+
+    week_index = int(week_num) - 1
+    week_scores = [] # empty list for all scores
+    
+    for player in range(0, 12):
+        team = league.teams[player]
+        week_scores.append(team.scores[week_index])
+
+    return week_scores
+
+def getpointsfor():
+    '''update points_for'''
+
+    pf_list = []
+    # update points_for for all 12 owners
+    for n in range(0, 12):
+        team = league.teams[n]
+        pf_list.append(team.points_for)
+
+    return pf_list
+        
+
+
+def getallownerscores(ownerID):
+    '''Get All Scores From a Single Owner'''
+
+    ownerID -= 1
+    team = league.teams[ownerID]
+    allownerscores = team.scores
+    teamname = team.team_name
+    print(teamname)
+
+    return allownerscores
+
+def getallteamwins():
+    '''Return a list of h2h wins'''
+    team = league.teams
+
+    return team
